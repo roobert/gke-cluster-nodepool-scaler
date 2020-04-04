ccc06c0d7e448812bc5040f3947cd05ff4031c69
Rob Wilson <roobert@gmail.com>
2020-04-04 19:10:14 +0100

    Add helper script

---
+++ scale_nodepools.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+NODE_COUNT="${1}"
+PROJECTS="${2}"
+TOPIC="gke-cluster-nodepool-scaler"
+
+set -euo pipefail
+
+if [[ $# != 2 ]]; then
+  echo "usage: $0 <node count> \"<project> ...\""
+  exit 1
+fi
+
+for project in ${PROJECTS}; do
+  gcloud --project "${PROJECT}" pubsub topics publish "${TOPIC}" --message "{\"nodes\":${NODE_COUNT}}"
+done
